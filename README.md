6f52a921b6ba708c0e28163c9befc789e37d03c7
Fuyuan Wang <zoeblow@gmail.com>
2024-12-12 15:09:28 +0800

    chore: The first time commit code.

---
+++ README.md
@@ -0,0 +1,13 @@
+# ✨ ❤️ Emoji-Cheat-Sheet Emoji 备忘录 ❤️ ✨
+
+在 GitHub 上发现了这个玩意，由于本人是个聊天不发表情会死星人。Git 提交也不例外，所以就下载了 GitHub 上面的文件部署到Pages上，方便使用。
+
+打开页面 [pages](https://emoji.maozi.io/)
+
+选择想要的表情点击一下就好了，他会直接进入到你的粘贴板。
+
+## 版本信息
+
+根据  <https://github.com/WebpageFX/emoji-cheat-sheet.com> 代码修改而来 如果有侵权请联系我删除，谢谢！
+
+This page was created by [Arvid Andersson](http://arvidandersson.se) . Source code is available on [GitHub](https://GitHub.com/WebpageFX/emoji-cheat-sheet.com).
---
+++ script/script.js
@@ -0,0 +1,162 @@
+/*
+ * Emoji cheat sheet
+ */
+const FX = {};
+FX.version = "0.1.0";
+FX.BackToTop = {
+  $btn: null,
+
+  init() {
+    this.$btn = $(".back-to-top");
+
+    if (this.$btn.length) {
+      this.bind();
+    }
+  },
+
+  bind() {
+    $(window).on("scroll load", this.maybeShowButton.bind(this));
+    this.$btn.on("click", this.scrollToTop);
+  },
+
+  maybeShowButton() {
+    if ($(window).scrollTop() > 500) {
+      this.$btn.fadeIn();
+    } else {
+      this.$btn.fadeOut();
+    }
+  },
+
+  scrollToTop() {
+    $(window).scrollTop(0);
+  },
+};
+
+$(document).ready(function () {
+  try {
+    if (document.flashtest && document.flashtest.PercentLoaded() >= 0) {
+      // Flash was able to load the test swf and is working
+      initZeroClipboard();
+    } else {
+      initJsClipboard();
+    }
+  } catch (e) {
+    initJsClipboard();
+  }
+
+  function initZeroClipboard() {
+    ZeroClipboard.config({
+      forceHandCursor: true,
+      hoverClass: "hover",
+    });
+    var clipboardclient = new ZeroClipboard();
+
+    clipboardclient.on("ready", function (readyEvent) {
+      $("ul").on("mouseover", "div", function () {
+        try {
+          clipboardclient.clip(this);
+        } catch (e) {}
+      });
+
+      clipboardclient.on("copy", function (evt) {
+        var clipboard = evt.clipboardData;
+        clipboard.setData("text/plain", $(evt.target).text().trim());
+      });
+
+      clipboardclient.on("aftercopy", function (evt) {
+        var highlightedElement = evt.target;
+        $(highlightedElement).addClass("copied");
+        setTimeout(function () {
+          $(highlightedElement).removeClass("copied");
+        }, 800);
+      });
+    });
+
+    clipboardclient.on("error", function (event) {
+      ZeroClipboard.destroy();
+      initJsClipboard();
+    });
+  }
+
+  var jsClipboardSupported = true; // we can't check if this is true unless the user tries once
+  function initJsClipboard() {
+    $("ul").on("click", "div", function () {
+      try {
+        if (jsClipboardSupported) {
+          var selection = getSelection();
+          selection.removeAllRanges();
+
+          var range = document.createRange();
+          range.selectNodeContents(this);
+          selection.addRange(range);
+
+          var highlightedElement = $(this);
+          if (document.execCommand("copy") == true) {
+            // this will silently fail on IE11 when access is denied
+            $(highlightedElement).addClass("copied");
+            setTimeout(function () {
+              $(highlightedElement).removeClass("copied");
+            }, 800);
+
+            // console.log(highlightedElement, "highlightedElement");
+          } else {
+            // copying was not successfull or denied by the user or browser preferences
+            // see Firefox about:config "dom.allow_cut_copy"
+            $(highlightedElement).addClass("clipboardError");
+            setTimeout(function () {
+              $(highlightedElement).removeClass("clipboardError");
+            }, 6000);
+
+            jsClipboardSupported = false;
+          }
+          selection.removeAllRanges();
+        }
+      } catch (e) {}
+    });
+  }
+
+  function isElementMatching(element, needle) {
+    var alternative = element.attr("data-alternative-name");
+    return (
+      $(element).text().toLowerCase().indexOf(needle) >= 0 ||
+      (alternative != null && alternative.toLowerCase().indexOf(needle) >= 0)
+    );
+  }
+
+  function highlightElements(needle) {
+    if (needle.length == 0) {
+      highlightAll();
+      return;
+    }
+    needle = needle.toLowerCase();
+    $(".emojis li").each(function (index, el) {
+      if (isElementMatching($(".name", el), needle)) {
+        $(el).show();
+      } else {
+        $(el).hide();
+      }
+    });
+  }
+
+  function highlightAll() {
+    $(".emojis li").show();
+  }
+
+  $("#header .search").keyup(function (e) {
+    if (e.keyCode == 27) {
+      // ESC
+      $(this).val("").blur();
+      highlightAll();
+    }
+  });
+  $("#header .search").on("change paste keyup", function () {
+    highlightElements($("#header .search").val());
+  });
+  $("#header .search").focus();
+
+  FX.BackToTop.init();
+  console.log(FX, "FX");
+
+  console.log($("img.lazy").length, "images");
+  $("img.lazy").lazyload();
+});
